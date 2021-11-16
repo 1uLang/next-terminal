21d16d033989ee6bdc605eacb5612c3f9aa654c9
lulang <lulang@zhiannet.com>
2021-11-16 22:32:54 +0800

    clear

--- build.sh
+++
@@ -1,7 +0,0 @@
-cd web
-npm run build
-rm -rf ../bin/web/build
-mkdir -p ../bin/web/build
-cp -r build ../bin/web/
-cd ..
-CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -o bin/next-terminal main.go
--- web/src/common/constants.js
+++
@@ -1,7 +0,0 @@
-export const PROTOCOL_COLORS = {
-    'rdp': 'cyan',
-    'ssh': 'blue',
-    'telnet': 'geekblue',
-    'vnc': 'purple',
-    'kubernetes': 'volcano'
-}
